3db9b037bc84fb6ae1265380b9304f8831191090
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-12 12:08:40 +0800

    refactor: express routes

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
+const session = require('express-session')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+const routes = require('./routes')
 const app = express()
 const PORT = 3000
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+usePassport(app)
 app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
-app.get('/', (req,res) => {
-  res.send('hello world')
-})
+app.use(routes)
+
+
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
