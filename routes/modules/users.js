71ac33a39b43b1ae8fa913ae91633a9c72408978
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-12 12:31:48 +0800

    fix: message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,11 +22,26 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+
+  if(!name || !email || !password || !confirmPassword) {
+    errors.push({ msg: '所有欄位都是必填'})
+  }
+if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
 
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -49,6 +64,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '登出成功')
   res.redirect('/users/login')
 })
 
